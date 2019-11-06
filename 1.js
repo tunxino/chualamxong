7c70671983e3fe94bdaba7939251e77121fb80ef
tunxino <tunxino@gmail.com>
2019-11-06 13:14:37 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function() {
+
+
+
+    $hieuung = $('.noidung').isotope({
+        // options
+        itemSelector: '.khoianh',
+        layoutMode: 'masonry'
+    });
+    $hieuung.imagesLoaded().progress(function() {
+        $hieuung.isotope('layout');
+    });
+
+
+
+    $(window).scroll(function(event) {
+        // console.log('xu ly');
+        // console.log($(this).scrollTop());
+        var vitri = $(this).scrollTop();
+        if (vitri >= 500) {
+            console.log('dc r')
+            $('.menu').addClass('bienhinh');
+        } else {
+            $('.menu').removeClass('bienhinh');
+        }
+    });
+})
